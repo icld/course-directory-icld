0318d585f644225893d9959ad692fa940834c6df
icld <ian@icldesign.com>
2021-05-11 13:06:15 -0700

    add featured teacher, and link to /

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,8 @@ import Home from './Home';
 import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
-import NotFound from './NotFound'
+import NotFound from './NotFound';
+import Featured from './Featured';
 
 const App = () => (
   <BrowserRouter>
@@ -21,7 +22,8 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path='/about' render={() => <About title='About' />} />
-        <Route path='/teachers' component={Teachers} />
+        <Route exact path='/teachers' component={Teachers} />
+        <Route path='/teachers/:topic/:fname-:lname' component={Featured} />
         <Route path='/courses' component={Courses} />
         <Route component={NotFound} />
       </Switch>
--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Featured = () => {
+const Featured = ({ match }) => {
+
+  let name = `${match.params.fname} ${match.params.lname}`;
+  let topic = match.params.topic;
 
   return (
     <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+      <h2>{name} </h2>
+      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
+
+      <NavLink to="/">Home</NavLink>
+
     </div>
   );
 }
