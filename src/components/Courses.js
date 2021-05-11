e6cbcf498993b490a480745685923f57e35d291a
icld <ian@icldesign.com>
2021-05-11 11:53:43 -0700

    add routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
   Route,
-  NavLink
+  NavLink,
+  Redirect
 } from 'react-router-dom';
 
 
@@ -10,20 +11,22 @@ import CSS from './courses/CSS'
 import HTML from './courses/HTML'
 import JavaScript from './courses/JavaScript'
 
-const Courses = () => (
+const Courses = ({ match }) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
-        <li><NavLink to='/courses/html'>HTML</NavLink></li>
-        <li><NavLink to='/courses/css'>CSS</NavLink></li>
-        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
 
-    <Route path='/courses/html' component={HTML}></Route>
-    <Route path='/courses/css' component={CSS}></Route>
-    <Route path='/courses/javascript' component={JavaScript}></Route>
+
+    <Route exact path={match.path} render={() => <Redirect to={`${match.path}/html`} />}></Route>
+    <Route path={`${match.path}/html`} component={HTML}></Route>
+    <Route path={`${match.path}/css`} component={CSS}></Route>
+    <Route path={`${match.path}/javascript`} component={JavaScript}></Route>
 
   </div>
 );
